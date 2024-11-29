9e862ca486f9b14cfcf655a5ef0673d0ce8bd6d8
victeams <72262085+victeams@users.noreply.github.com>
2024-11-29 14:12:35 +0100

    Add files via upload
    Signed-off-by: victeams <72262085+victeams@users.noreply.github.com>

---
+++ ce4dfa68de06be68.fr-FR.js
@@ -0,0 +1,2 @@
+window['cmsg']['assets'] = window['cmsg']['assets'] || {};
+window['cmsg']['assets']["fr-FR"] = Object.assign(window['cmsg']['assets']["fr-FR"] || {}, {"5706":{"js":["35ba34d00cb1fc23.strings.js"],"css":[]},"8701":{"js":["f9f90d671b5d5495.strings.js"],"css":[]}});
